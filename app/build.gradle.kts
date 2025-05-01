a0e783d58c3968f222cdcb066a7acecf98fa89f0
Muhammad Farhan Fadlillah <ffadlillah18@gmail.com>
2025-05-01 23:14:40 +0700

    Menambahkan Navigation Component

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigation.compose)
 //    implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
     testImplementation(libs.junit)
