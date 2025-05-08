e7d9d29858f3e7e716450db7f9d8019d91dc26b8
Muhammad Farhan Fadlillah <ffadlillah18@gmail.com>
2025-05-08 21:20:54 +0700

    Mengubah warna topbar

--- app/src/main/java/com/farhanfad0036/remindlist/ui/theme/screen/RecycleBinScreen.kt
+++ app/src/main/java/com/farhanfad0036/remindlist/ui/theme/screen/RecycleBinScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -67,14 +68,15 @@ fun RecycleBinScreen(
                     Text(text = "Data terhapus")
                 },
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
-                    containerColor = Color.DarkGray,
+                    containerColor = Color.Red,
                     titleContentColor = Color.White
                 ),
                 navigationIcon = {
                     IconButton(onClick = { navController.navigateUp() }) {
                         Icon(
                             imageVector = Icons.Default.ArrowBack,
-                            contentDescription = "Kembali"
+                            contentDescription = stringResource(R.string.kembali),
+                            tint = MaterialTheme.colorScheme.primary
                         )
                     }
                 }
